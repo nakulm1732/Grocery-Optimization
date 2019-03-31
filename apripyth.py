7335da8a8ce44c0e4a9a5876350c267c3afc578e
nakulm1732 <48866000+nakulm1732@users.noreply.github.com>
2019-03-31 02:26:07 -0500

    Add files via upload

---
+++ apripyth.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 22 02:03:48 2018
+
+@author: nakul
+"""
+
+#Apriori Algorithm
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+dataset = pd.read_csv('Market_Basket_Optimisation.csv', header = None)
+transactions = []
+for i in range(0, 7501):
+    transactions.append([str(dataset.values[i,j]) for j in range(0, 20)])
+    
+# Training Apriori on the dataset
+from apyori import apriori
+rules = apriori(transactions, min_support = 0.003, min_confidence = 0.2, min_lift = 3, min_length = 2)
+
+#Visualizing the Result
+results = list(rules)
